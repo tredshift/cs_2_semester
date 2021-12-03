d77a5a52bf1717c12d49caff3e6c787b255935d5
tredshift <81083350+tredshift@users.noreply.github.com>
2021-12-04 01:56:28 +0300

    Update 3.3.cpp

--- week3/3.3.cpp
+++ week3/3.3.cpp
@@ -1,34 +1,18 @@
-#include <iostream>
+#include <functional>
 #include <vector>
-#include <algorithm>
-
-using namespace std;
-int main()
-{
-    int n, unit;
-    vector <int> array, array_incresing, array_decreasing;
-    cout << "Кол-во элементов в массиве: ";
-    cin >> n;
-    cout << "Массив: ";
-    for (auto i = 0; i < n; ++i)
-    {
-        cin >> unit;
-        array.push_back(unit);
+#include <iostream>
+int main(){
+    std::vector<int> s = {2, 6, 8,0, 15, 31, -5};
+    std::sort(s.begin(), s.end(), [](int a, int b) {
+        return a > b;
+    });
+    for(int i = 0; i < 7; i++){
+        std::cout << s[i] << std::endl;
     }
-    array_incresing = array;
-    sort(begin(array_incresing), end(array_incresing));
-    for (int i : array_incresing)
-    {
-        cout << i << '\n';
+    std::sort(s.begin(), s.end(), [](int a, int b) {
+        return a < b;
+    });
+    for(int i = 0; i < 7; i++){
+        std::cout << s[i] << std::endl;
     }
-    array_decreasing = array;
-    sort(begin(array_decreasing), end(array_decreasing),
-              [](int x, int y) {return x > y; });
-
-    for (int i : array_decreasing)
-    {
-        cout << i << '\n';
-    }
-
-    return 0;
 }
